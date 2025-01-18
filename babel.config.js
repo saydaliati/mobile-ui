4daf640be6c374579d046791dfe13abd8c6d6593
Zaiidmo <Zaiidmoumnii@gmail.com>
2025-01-18 20:11:14 +0100

    Update Versions

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
+    api.cache(true);
+    return {
+      presets: ['babel-preset-expo'],
+      plugins: [
+        'nativewind/babel',
+        'react-native-reanimated/plugin',
+        [
+          'module-resolver',
+          {
+            alias: {
+              '@': './',
+            },
+          },
+        ],
+      ],
+    };
   };
-};
